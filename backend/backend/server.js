b4e1d9cc8ddecc37b6673405a4ed5bfc135f4bb7
Gokul-sami <gokulpandiyan6@gmail.com>
2025-10-14 21:30:58 +0530

    `Added mongoose dependency and MongoDB connection to server.js`

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 
-app.get('/api/message', () => {
-  res.json('Hello from Express!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch(err => {
+    console.error("MongoDB Connection error:", err.message);
+});
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
 });
 
 const PORT = process.env.PORT || 5000;
