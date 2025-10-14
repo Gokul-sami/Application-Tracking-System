1f4e5593fb6cb5a6a2232cea60ff235f8f1d8334
Gokul-sami <gokulpandiyan6@gmail.com>
2025-10-14 23:55:02 +0530

    `Added bcryptjs, cors, jsonwebtoken, signup and login api

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import authControllers from "./routes/authControllers.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const mongoURI = process.env.MONGODB_URI;
 
+// connnect to db
 mongoose.connect(mongoURI)
   .then(() => {
     console.log("MongoDB Connected Successfully");
@@ -19,9 +23,13 @@ mongoose.connect(mongoURI)
     console.error("MongoDB Connection error:", err.message);
 });
 
+// Test Route
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
+
+// signup and login routes
+app.use("/api/auth", authControllers);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
