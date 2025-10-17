e6f1d948732cd5fd29f53ad3827651de69e5766b
Gokul-sami <gokulpandiyan6@gmail.com>
2025-10-17 06:49:01 +0530

    css file path issue resolved

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/login.css";
+import "./styles/login.css";
 import Prism from "../components/Prism";
 
 const Login = () => {
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/signup.css";
+import "./styles/signup.css";
 import Prism from "../components/Prism";
 
 const Signup = () => {
